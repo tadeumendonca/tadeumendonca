45e00a1d871aca5903db42d3733b6b5bed6c0cfc
Tadeu Mendonça <tadeu.mendonca@g.globo>
2020-09-30 19:49:48 -0300

    Fix

--- upload-site.sh
+++ upload-site.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-aws s3 cp ./home s3://tadeumendonca.me --recursive --exclude ".git/*,blog.html,.gitignore"
+aws s3 cp ./home s3://tadeumendonca.me --recursive --exclude ".git/*,blog.html,.gitignore,.DS_Store"
